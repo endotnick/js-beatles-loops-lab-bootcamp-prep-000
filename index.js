89812389f791e803867303940168e44e6a1a708b
Nick Rodriguez <nickbenrodriguez@gmail.com>
2018-10-12 22:42:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 // add solution here
 const theBeatlesPlay = (musicians, instruments) => {
-
+  const beatles = [];
+  for (let i = 0; i < musicians.length; i++) {
+    beatles.push(`${musicians[i]} plays ${instruments[i]}`);
+  }
+  return beatles;
 };
 
 const johnLennonFacts = (facts) => {
-  
+
 };
 
 const iLoveTheBeatles = (n) => {
